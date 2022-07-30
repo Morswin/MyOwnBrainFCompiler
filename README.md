d0acb02ae7db2f99a5db979a4ec8b97bcbdaae59
Morswin <tomaszpowezk4@gmail.com>
2022-07-30 15:34:59 +0200

    WIP - Some more setters and getters, and a few placeholders

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# MyOwnBrainFCompiler
+# MyOwnBrainFInterpreter
 I just want to try out unittests in python, and this would be a nice way to do so.
+Sorry for mistaking compiler with interpreter. At least I've fixed it in the "Readme.md".
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import List
+from stack import Stack
 # TODO take an BF input
 # TODO translate from BD to ASCII
 # TODO display the result
@@ -6,6 +7,7 @@ from typing import List
 # TODO don't forget to use typing correctly
 # TODO write PEP-8 correct code
 # TODO write it all in a defensive way
+# TODO Add feature to use it from command line with instant reply
 
 
 class Main:
@@ -16,19 +18,52 @@ class Main:
     I know that I shouldn't do this in one file, but that's not what I wanted to train
         in here.
     """
+    _acceptable_chars: str = "<>[]-+,."
     _char_array: List[str] = []
     _char_array_max_length: int = 30_000
     _char_array_pointer: int = 0
-    _code_in: str = ""                       # For storing input
+    _code_in: List[str] = []                 # For storing input
     _code_out: str = ""                      # For storing the result
 
     def __init__(self, max_length: int = 30_000) -> None:
         self.char_array_max_length = max_length
-        self.char_array = ["" for _ in range(self.char_array_max_length)]
+        self.char_array = [" " for _ in range(self.char_array_max_length)]
 
     def __call__(self, *args, **kwargs) -> None:
         print("It kind of works")
         raise NotImplementedError("Work in progress...")
+
+    # some placeholders here
+    def _1(self):
+        pass  # >
+
+    def _2(self):
+        pass  # <
+
+    def _3(self):
+        pass  # +
+
+    def _4(self):
+        pass  # -
+
+    def _5(self):
+        pass  # .
+
+    def _6(self):
+        pass  # ,
+
+    def _7(self):
+        pass  # [
+
+    def _8(self):
+        pass  # ]
+
+    def get_input(self) -> None:
+        self.code_in = [_ for _ in input().strip()]
+
+    @property
+    def acceptable_chars(self) -> str:
+        return self._acceptable_chars
 
     @property
     def char_array(self) -> List[str]:
@@ -71,6 +106,31 @@ class Main:
             )
         else:
             self._char_array_max_length = length
+
+    @property
+    def code_in(self) -> List[str]:
+        return self._code_in
+
+    @code_in.setter
+    def code_in(self, code: List[str]) -> None:
+        if not isinstance(code, list):
+            raise TypeError(
+                "Code in takes only a List of characters (str)."
+            )
+        elif not all([isinstance(_, str) for _ in code]):
+            raise TypeError(
+                "In \"code_in\", all characters must be of type str."
+            )
+        elif not all([len(_) != 1 for _ in code]):
+            raise ValueError(
+                "In \"code_in\", all characters must be of length 1."
+            )
+        elif not all([_ in self.acceptable_chars for _ in code]):
+            raise ValueError(
+                "BF code was provided with an illegal character."
+            )
+        else:
+            self._code_in = code
 
 
 if __name__ == "__main__":
