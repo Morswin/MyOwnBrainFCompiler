da9fbceb0740d93e128cd06388335784fee59f2d
Morswin <tomaszpowezk4@gmail.com>
2022-07-30 12:34:34 +0200

    Initial commit and setup of enviroment

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MyOwnBrainFCompiler
+I just want to try out unittests in python, and this would be a nice way to do so.
---
+++ main.py
@@ -0,0 +1,6 @@
+# TODO take an BF input
+# TODO translate from BD to ASCII
+# TODO display the result
+# TODO create unittests for this project
+# TODO don't forget to use typing correctly
+# TODO write PEP-8 correct code
